b909db951ae7b3b12a22bec863d143bcbaa654a3
bigsizeJ <apacjessie@gmail.com>
2023-02-02 14:04:40 +0800

    finished merge sort

--- index.js
+++ index.js
@@ -15,5 +15,29 @@ const fibRecursive = (n) => {
   return [...arr, arr[n - 2] + arr[n - 3]];
 };
 
+const MergeSort = (arr) => {
+  if (arr.length <= 1) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+  const left = MergeSort(arr.slice(0, mid));
+  const right = MergeSort(arr.slice(mid));
+
+  return merge(left, right);
+};
+
+const merge = (leftArr, rightArr) => {
+  const arr = [];
+
+  while (leftArr.length > 0 && rightArr.length > 0) {
+    const minIndex = leftArr[0] < rightArr[0] ? leftArr : rightArr;
+    const sortedArr = minIndex.shift();
+    arr.push(sortedArr);
+  }
+
+  return arr.concat(leftArr, rightArr);
+};
+
 console.log(fibIterative(8));
 console.log(fibRecursive(8));
+
+console.log(MergeSort([531, 1245, 12, 551, 64]));
